1035e1fec55fc1eaac14d1a999b889024c0a11bd
Juan Pablo <jpmonteagudo2014@gmail.com>
2025-02-13 12:20:05 -0500

    edit badges and workflow

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,1 @@
-utils::globalVariables(c("author_data","rasb_bible","new_testament","old_testament","book"))
+utils::globalVariables(c("author_data","rasb_bible","new_testament","old_testament","book","`%||%`"))
