feeecc97b287a139233185ece61f2b6a3033bd45
Juan Pablo <jpmonteagudo2014@gmail.com>
2025-02-13 15:42:41 -0500

    new changes

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-utils::globalVariables(c("author_data","rasb_bible","new_testament","old_testament","book","%||%"))
-=======
 utils::globalVariables(c("author_data","rasb_bible","new_testament","old_testament","book"))
->>>>>>> parent of 1035e1f (edit badges and workflow)
+
